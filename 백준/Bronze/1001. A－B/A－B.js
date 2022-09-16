1d16b6cb71ee355d72763f26aad0f5650d59df3d
Kyle <111362079+JunTaeHahm@users.noreply.github.com>
2022-09-16 09:19:32 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,9 @@
+// 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+const fs = require("fs");
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '.input.txt';
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+input = input[0];
+input = input.split(" ").map((item) => +item);
+
+console.log(input[0]-input[1]);
